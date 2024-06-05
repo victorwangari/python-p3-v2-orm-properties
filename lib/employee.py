35c04433957332437ceb47b01c1fc8249575cef1
Victor kamau <Victor Wangari>
2024-06-05 11:43:52 +0300

    initial commit

--- lib/employee.py
+++ lib/employee.py
@@ -19,6 +19,36 @@ class Employee:
             f"<Employee {self.id}: {self.name}, {self.job_title}, " +
             f"Department ID: {self.department_id}>"
         )
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len (name):
+            self._name = name
+
+        else:
+            raise ValueError("Name must be a non-empty string")
+    @property
+    def job_title(self):
+        return self._job_title
+    @job_title.setter
+    def job_title(self, job_title):
+        if isinstance(job_title, str) and len(job_title):\
+            self._job_title = job_title
+            
+        else:
+            raise ValueError("Job title must be a non-empty string")
+    @property
+    def department_id(self):
+        return self._department_id
+    @department_id.setter
+    def department_id(self, department_id):
+        if type (department_id) is int and Department.find_by_id(department_id):
+            self._department_id = department_id
+        else:
+            raise ValueError("Department ID must be a valid integer")
+        
 
     @classmethod
     def create_table(cls):
